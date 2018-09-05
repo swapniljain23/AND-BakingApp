a46dac4cc8f6fd3e0fd54a0c988fdadadb6f62a0
Swapnil Jain <jainswapnil@jainswapnil-macbookpro.roam.corp.google.com>
2018-09-04 18:13:35 -0700

    Fix to run main activity tests.

--- app/src/androidTest/java/com/swapniljain/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/com/swapniljain/bakingapp/MainActivityTest.java
@@ -1,20 +1,32 @@
 package com.swapniljain.bakingapp;
 
+import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingRegistry;
 import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.swapniljain.bakingapp.activity.MainActivity;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.internal.matchers.TypeSafeMatcher;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.startsWith;
@@ -36,9 +48,31 @@ public class MainActivityTest {
 
     @Test
     public void loadRecipe() {
-        onData(anything()).inAdapterView(withId(R.id.rv_recipe_list)).atPosition(0)
-                .onChildView(withId(R.id.tv_recipe_name))
-                .check(matches(withText(startsWith("Nu"))));
+
+        onView(ViewMatchers.withId(R.id.rv_recipe_list))
+                .perform(RecyclerViewActions.scrollToPosition(0));
+
+        onView(ViewMatchers.withId(R.id.rv_recipe_list))
+                .check(matches(hasDescendant(withText("Nutella Pie"))));
+
+        onView(ViewMatchers.withId(R.id.rv_recipe_list))
+                .perform(RecyclerViewActions.scrollToPosition(1));
+
+        onView(ViewMatchers.withId(R.id.rv_recipe_list))
+                .check(matches(hasDescendant(withText("Brownies"))));
+
+        onView(ViewMatchers.withId(R.id.rv_recipe_list))
+                .perform(RecyclerViewActions.scrollToPosition(2));
+
+        onView(ViewMatchers.withId(R.id.rv_recipe_list))
+                .check(matches(hasDescendant(withText("Yellow Cake"))));
+
+        onView(ViewMatchers.withId(R.id.rv_recipe_list))
+                .perform(RecyclerViewActions.scrollToPosition(3));
+
+        onView(ViewMatchers.withId(R.id.rv_recipe_list))
+                .check(matches(hasDescendant(withText("Cheesecake"))));
+
     }
 
     @After
